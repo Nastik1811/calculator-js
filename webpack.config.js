af842569ec40d9dc1e93691e2aaa3453f03cbcde
Nastik1811 <lugovskaya.1999@mail.ru>
2021-10-07 12:55:19 +0300

    Setup pre-commit hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,22 @@
-const path = require("path")
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = (env, argv) => {
     const isDev = argv.mode === 'development'
 
-    const filename = (ext) => isDev ? `[name].bundle.${ext}` : `[name].[contenthash].bundle.${ext}` 
+    const filename = (ext) =>
+        isDev ? `[name].bundle.${ext}` : `[name].[contenthash].bundle.${ext}`
 
     const plugins = () => {
         const base = [
             new HtmlWebpackPlugin({
-                template: './index.html'
-            }), 
-            new MiniCssExtractPlugin({
-                filename: filename('css')
+                template: './index.html',
             }),
-            new FaviconsWebpackPlugin({
-                logo: './favicon.png',
-                favicons: {
-                    appName: 'calculator-js',
-                    appDescription: 'My Calculator',
-                    developerName: 'Anastasia Lugovskaya',
-                    developerURL: null,
-                    icons: {
-                        coast: false,
-                        yandex: false
-                    }
-                }
-
-            })
+            new MiniCssExtractPlugin({
+                filename: filename('css'),
+            }),
         ]
 
         if (isDev) {
@@ -43,28 +29,25 @@ module.exports = (env, argv) => {
     return {
         context: path.resolve(__dirname, 'src'),
         entry: {
-            main: './index.js'
+            main: './index.js',
         },
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: filename('js'),
-            clean: true
+            clean: true,
         },
         plugins: plugins(),
         devtool: isDev ? 'source-map' : false,
         devServer: {
-            port: 4200
+            port: 4200,
         },
         module: {
             rules: [
                 {
                     test: /\.s[ac]ss$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader'
-                    ]
-                }
-            ]
-        }
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                },
+            ],
+        },
     }
 }
